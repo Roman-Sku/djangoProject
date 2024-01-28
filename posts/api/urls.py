baef69679976f1ef9fabd6ff4501c0a3f0d3aeb6
Roman_Sku <romanskuratovich08@gmail.com>
2024-01-28 19:12:53 +0300

    28-29

--- posts/api/urls.py
+++ posts/api/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("posts/", views.NoteListCreateAPIView.as_view(), name="note-list-create"),
     path("posts/<uuid:pk>", views.NoteDetailAPIView.as_view(), name="note"),
     path("posts/image", views.UploadImageAPIView.as_view(), name="note-image-upload"),
-    path("posts/tags", views.TagListCreateApiView.as_view(), name="tag")
+    path("tags/", views.TagListCreateApiView.as_view(), name="tag")
 
 ]
